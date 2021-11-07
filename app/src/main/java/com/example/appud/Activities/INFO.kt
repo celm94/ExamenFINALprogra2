b6d808d0d70284fa0459916881ea0d62baa44642
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-11-07 06:37:41 -0600

    INFO agregada

---
+++ app/src/main/java/com/example/appud/Activities/INFO.kt
@@ -0,0 +1,20 @@
+package com.example.appud.Activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appud.R
+import kotlinx.android.synthetic.main.activity_info.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class INFO : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+
+        regresarButton.setOnClickListener{
+            val intent2 = Intent(this, MainActivity::class.java)
+            startActivity(intent2)
+        }
+    }
+}
--- app/src/main/java/com/example/appud/Activities/MainActivity.kt
+++ app/src/main/java/com/example/appud/Activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.appud.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.appud.R
@@ -35,10 +37,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nueve: Button
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         InicializarComponentes()
+
+        butINFO.setOnClickListener{
+            val intent = Intent(this, INFO::class.java)
+            startActivity(intent)
+        }
+
+
 
 
 
